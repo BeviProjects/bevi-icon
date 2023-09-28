a453c32014c117a170f681f02bd79862eca40a2d
andre.aguiar <andre.aguiar@bevicred.com.br>
2023-09-28 16:39:05 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import BvIcon from '.'
+
+const App = () => {
+
+  return (
+    <h1>Bevi Icon - <BvIcon name='cube' variant='duo' size={4} title='Cube'/></h1>
+  )
+}
+
+export default App
