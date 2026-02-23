7aa41250702138c4b83ca553d1fc1968e9bbc115
andre.aguiar <andreluizmagjr@outlook.com>
2026-02-23 09:59:19 -0300

    Added clt icon

---
+++ src/data/v2.0.ts
@@ -0,0 +1,20 @@
+export const v2_0 = [
+	{
+		id: 'clt',
+		name: 'CLT',
+		displayName: 'clt',
+		status: 'active',
+		tags: ['governo', 'work', 'wallet'],
+		variants: {
+			variant: ['solid', 'duo', 'dark', 'light'],
+			weight: [400],
+		},
+		metadata: {
+			createdAt: '2026-02-23',
+			updatedAt: '2026-02-23',
+			addedIn: '2.0.4',
+			updatedIn: '2.0.4',
+			iconVersion: '2.0',
+		},
+	},
+]
