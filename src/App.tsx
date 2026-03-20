ed232a9c9d5356d5fdd6b67231a679bd8c3eaa2d
andreluizmagjr <andreluizmagjr@outlook.com>
2026-03-20 14:29:54 -0300

    Added open-in-full-600 icon

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
 	return (
 		<section>
 			<h1>Welcome to Bevi icon</h1>
-			<BvIcon name="attach" variant="solid" weight={600} width={256} />
+			<BvIcon name="openInFull" weight={600} width={256} />
 		</section>
 	);
 };
